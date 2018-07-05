a657623b55c2c4eef77d58a6b159a6439c92c46b
Isaac <jojosep@microsoft.com>
2018-07-05 16:14:30 -0700

    rename to Agent.cs

--- RecurringTasksAgent/Agent.cs
+++ RecurringTasksAgent/Agent.cs
@@ -7,12 +7,12 @@ using Microsoft.Azure.IoTSolutions.IotHubManager.Services.Diagnostics;
 
 namespace Microsoft.Azure.IoTSolutions.IotHubManager.RecurringTasksAgent
 {
-    public interface IRecurringTasksAgent
+    public interface IAgent
     {
         void Run();
     }
 
-    public class RecurringTasksAgent : IRecurringTasksAgent
+    public class Agent : IAgent
     {
         // When cache initialization fails, retry in few seconds
         private const int CACHE_INIT_RETRY_SECS = 10;
@@ -26,7 +26,7 @@ namespace Microsoft.Azure.IoTSolutions.IotHubManager.RecurringTasksAgent
         private readonly IDeviceProperties deviceProperties;
         private readonly ILogger log;
 
-        public RecurringTasksAgent(
+        public Agent(
             IDeviceProperties deviceProperties,
             ILogger logger)
         {
